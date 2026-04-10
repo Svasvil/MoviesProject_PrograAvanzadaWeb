90472a5b5457a55bf6afbab8624175e7a35388d4
Sebastian Vasquez <svasvil0211@gmail.com>
2026-04-10 12:45:25 -0600

    Add project files.

---
+++ Movies.API/DatabasesConnections/ObjContex.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Movies.API.Models;
+
+namespace Movies.API.DatabasesConnections
+{
+    public class ObjContex : DbContext
+    {
+        public ObjContex(DbContextOptions<ObjContex> opts) : base(opts) { }
+
+        public DbSet<MovieModel> Movie { get; set; } = null!;
+    }
+
+    
+    }
---
+++ Movies.API/Models/MovieModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Movies.API.Models
+{
+    public class MovieModel
+    {
+        //Estos datos son los que agarre con un get de la APi de The Movie DB, por eso se llaman asi, para que se mapeen bien
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Overview { get; set; }
+        public string Release_Date { get; set; }
+        public string Poster_Path { get; set; }
+        public string Backdrop_Path { get; set; }
+        public double Vote_Average { get; set; }
+    }
+}
