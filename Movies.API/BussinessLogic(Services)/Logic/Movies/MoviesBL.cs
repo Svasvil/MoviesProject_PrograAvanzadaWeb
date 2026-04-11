3e5d8bc82342a719df28bf06f5eb7e80c626952c
Sebastian Vasquez <svasvil0211@gmail.com>
2026-04-11 14:35:50 -0600

    codigo de bl y demas

---
+++ Movies.API/BussinessLogic(Services)/Logic/Movies/MoviesBL.cs
@@ -0,0 +1,34 @@
+﻿using Movies.API.DataAccess_Repository_.Interfaces.Movies;
+using Movies.API.DTOs.Movies;
+/*
+ 
+namespace Movies.API.BussinessLogic_Services_.Logic.Movies
+{
+    public class MoviesBL //: //IMoviesBL
+    {
+        private readonly IMoviesDA _MoviesDA;
+     public MoviesBL(IMoviesDA moviesDA)
+        {
+            _MoviesDA = moviesDA;
+        }
+        
+        public async Task<MovieDTO> AddMovie(CreateMovieDTO movie) {
+            var newMovie = await _MoviesDA.AddMovie(movie);
+            return new MovieDTO
+            {
+                Id = newMovie.Id,
+                Title = newMovie.Title,
+                Overview = newMovie.Overview,
+                Release_Date = newMovie.Release_Date,
+                Poster_Path = newMovie.Poster_Path,
+                Backdrop_Path = newMovie.Backdrop_Path,
+                Vote_Average = newMovie.Vote_Average
+            };
+                
+
+
+        }
+    }
+
+}
+*/
---
+++ Movies.API/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movies.API.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
