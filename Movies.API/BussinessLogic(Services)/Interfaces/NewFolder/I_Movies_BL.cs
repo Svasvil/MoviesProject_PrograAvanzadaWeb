a399a48d55f51513c369b383cb0afa90acd60de7
Sebastian Vasquez <svasvil0211@gmail.com>
2026-04-11 14:44:35 -0600

    fix bl

---
+++ Movies.API/BussinessLogic(Services)/Interfaces/NewFolder/I_Movies_BL.cs
@@ -0,0 +1,6 @@
+﻿namespace Movies.API.BussinessLogic_Services_.Interfaces.NewFolder
+{
+    public interface I_Movies_BL
+    {
+    }
+}
---
+++ Movies.API/BussinessLogic(Services)/Logic/Movies/Movies_BL.cs
@@ -0,0 +1,8 @@
+﻿using Movies.API.BussinessLogic_Services_.Interfaces.NewFolder;
+
+namespace Movies.API.BussinessLogic_Services_.Logic.Movies
+{
+    public class Movies_BL //: I_Movies_BL
+    {
+    }
+}
